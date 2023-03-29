e7cfa8c9f6f1097dfd28e76a6c6e07242b8b0709
Marat Iskhakov <93031480+qrimosa@users.noreply.github.com>
2023-03-29 21:13:27 +0300

    Co-authored-by: RinatUA <RinatUA@users.noreply.github.com> Co-authored-by: andry432 <andry432@users.noreply.github.com> Co-authored-by: IWaithI <IWaithI@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import customtkinter
 import modules.creating_screen as m_screen
 import modules.small_bt
 import modules.big_bt
-
+import modules.frame_bt
+# первый трек - рикролллллллллллл
 
 m_screen.app.mainloop()
